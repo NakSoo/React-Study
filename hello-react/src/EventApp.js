abc9691e4b066e5a2ef38dfb41b8d2e816348041
Naksooroo <31647064+NakSoo@users.noreply.github.com>
2019-10-04 19:28:42 +0900

    6. Event

---
+++ hello-react/src/EventApp.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Event1 from './events/event1';
+import Event2 from './events/event2';
+import Event3 from './events/event3';
+import Event4 from './events/event4';
+import Event5 from './events/event5';
+import Event6 from './events/event6';
+import Event7 from './events/event7';
+import Event8 from './events/event8';
+
+function EventApp() {
+    return(
+        <>
+            <Event1/>
+            <Event2/>
+            <Event3/>
+            <Event4/>
+            <Event5/>
+            <Event6/>
+            <Event7/>
+            <Event8/>
+        </>
+    );
+}
+
+export default EventApp;
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ComponentApp from './ComponentApp';
+//import ComponentApp from './ComponentApp';
+import EventApp from './EventApp';
 
 //ReactDOM.render(<App />, document.getElementById('root'));  //babel 예제 따라하기
-ReactDOM.render(<ComponentApp/>, document.getElementById('root')); //Component 예제 따라하기
+//ReactDOM.render(<ComponentApp/>, document.getElementById('root')); //Component 예제 따라하기
+ReactDOM.render(<EventApp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
