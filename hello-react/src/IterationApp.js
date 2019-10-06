8e3abc11208251a98f9cb50f9ad0cf0ecc21499a
Naksooroo <31647064+NakSoo@users.noreply.github.com>
2019-10-06 22:29:16 +0900

    8. 컴포넌트 반복

---
+++ hello-react/src/IterationApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import KeySample from './iteration/iteration';
+import Iteration2 from './iteration/iteration2';
+
+function IterationApp() {
+    return(
+        <>
+            <KeySample/>
+            <Iteration2/>
+        </>
+    );
+}
+
+export default IterationApp;
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -5,13 +5,15 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 //import ComponentApp from './ComponentApp';
 //import EventApp from './EventApp';
-import ValidationApp from './ValidationApp';
+//import ValidationApp from './ValidationApp';
+import IterationApp from './IterationApp';
 
 
 //ReactDOM.render(<App />, document.getElementById('root'));  //babel 예제 따라하기
 //ReactDOM.render(<ComponentApp/>, document.getElementById('root')); //Component 예제 따라하기
 //ReactDOM.render(<EventApp/>, document.getElementById('root'));
-ReactDOM.render(<ValidationApp/>, document.getElementById('root'));
+//ReactDOM.render(<ValidationApp/>, document.getElementById('root'));
+ReactDOM.render(<IterationApp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
